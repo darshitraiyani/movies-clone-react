87448c3e2d7d1611ae40f1a69d194073afff6ae1
Darshit <darshitraiyani@gmail.com>
2025-07-02 11:50:56 +0530

    Build Generate

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/movies-clone-react/',
   plugins: [
     react(),
     tailwindcss()
